8486b36a0f18d8e961beb72e18c96bdd08056dd2
Lucas Cassiano Silva <lucascassiano@Lucass-MacBook-Pro.local>
2016-10-08 21:06:53 -0400

    Second screen added

---
+++ app/src/main/java/com/example/sneha/trails/SendDataActivity.java
@@ -0,0 +1,21 @@
+package com.example.sneha.trails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SendDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_data);
+    }
+
+    public void toggleSendData(View view)
+    {
+        //Sneha Do some cool stuff here ;)
+
+    }
+
+}
